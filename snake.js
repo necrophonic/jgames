74cb134d9d82c05c80d93e25c95de7116433c085
Jay <john@johngregory.me.uk>
2013-07-04 23:32:13 +0100

    Added initial debugging panel

--- snake.js
+++ snake.js
@@ -1,10 +1,12 @@
 var grid_size = 51; // Size for both X and Y axes
-
+var speed	  = 1000; // 1s
+var runner    = undefined; // Interval
 
 /* One time set up called on page load
 */
 function init() {
 	debug("Initialise");
+	set_up_grid();
 }
 
 // ----------------------------------------------------------------------------
@@ -26,6 +28,31 @@ function coords_to_id(x,y) {
 
 // ----------------------------------------------------------------------------
 
+function set_up_grid() {
+	debug("Set up grid");
+	var grid = document.getElementById("snake");
+
+	for (var y=0;y<grid_size;y++) {
+
+		var row = document.createElement("DIV");
+		row.setAttribute("class","row");
+
+		for (var x=0;x<grid_size;x++) {
+			
+			var cell = document.createElement("DIV");
+				cell.setAttribute("id",coords_to_id(x,y));
+				cell.setAttribute("class","cell");
+
+			row.appendChild(cell);
+
+		}
+		grid.appendChild(row);
+	}
+
+}
+
+// ----------------------------------------------------------------------------
+
 /* Walk the entire grid, row by row, left to right, and perform the given function
    on each cell as found.
 */
@@ -35,6 +62,8 @@ function walk_grid(func) {
 		for (var x=0;x<grid_size;x++) {
 
 			var cell_id = coords_to_id(x,y);
+			var cell    = document.getElementById(cell_id);
+			func(cell);
 
 		}
 	}
@@ -46,7 +75,7 @@ function walk_grid(func) {
 function debug(msg) {
 	var debug_area = document.getElementById("debug");	
 	var current_text = debug_area.innerHTML;	
-	current_text = msg + "<br>" + current_text;
+	current_text = '<div class="debug-msg">' + msg + "</div>" + current_text;
 	debug_area.innerHTML = current_text;
 	
 	return;
